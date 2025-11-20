4b6fb855abc75eb37bd9c499c1906b514b0b6fff
Pascal Martineau <pascal@lewebsimple.ca>
2025-11-20 13:02:57 -0500

    chore: Initial plugin

---
+++ wp-typesense.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: WP Typesense
+ * Plugin URI: https://github.com/lewebsimple/wp-typesense
+ * Description: Low-level Typesense integration for WordPress.
+ * Version: 0.2.0
+ * Author: Pascal Martineau <pascal@lewebsimple.ca>
+ * Author URI: https://websimple.com
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ * Text Domain: wp-typesense
+ * Domain Path: /languages
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
